eefd6b9243927a745c8c1e59ecaf902ad9b2d3ea
fred <jaubertf@yahoo.fr>
2022-10-06 14:38:06 +0200

    test

--- components/Accueil.js
+++ components/Accueil.js
@@ -2,26 +2,31 @@ import 'antd/dist/antd.css';
 import styles from '../styles/Accueil.module.css';
 import { Popover, Button } from 'antd';
 import Link from 'next/link';
+import { MiddlewareArray } from '@reduxjs/toolkit';
 
 
 function Accueil() {
   return (
-    <div className={styles.main}>
-      <div className={styles.gauche}>
-       <img src='twitter.png' className={styles.groslogo}/>
-      </div>  
-      <div className={styles.droite} >
+    <div className={styles.container}>
+        <div className={styles.contentleft}>
         <img src='twitter.png' className={styles.petitlogo}/>
-        <span>See what's</span>
-        <span>happening</span>
-        <span>Join Hackatweet today</span>
-        <button type="button" className={styles.boutonsignup}>Sign up</button>
-        <span>already have an account?</span>
-        <button type="button" className={styles.boutonsignin}>Sign in</button>
-        <Link href = "/accueil"> go to accueil</Link> 
-      </div>
+      <span>c'est du CACA    </span>
+        <button type="button" className={styles.btnlogout}>Logout</button>
+        </div>
+
+        <div className={styles.contentmilieu}>
+        <span>Home</span>
+        <button type="button" className={styles.btntweet}>tweet</button>
+        <div id="add-section"></div>
+        </div>
+
+        <div className={styles.contentright}>
+        <span>Trends</span>
+        </div>
     </div>
-  );
+   
+    );
 }
+
 
 export default Accueil;
